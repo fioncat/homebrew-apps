21574160cd606439c75cdaf7adf39c336cd30f57
fioncat <lazycat7706@gmail.com>
2024-12-26 15:25:21 +0800

    add csync formula

---
+++ Formula/csync.rb
@@ -0,0 +1,18 @@
+class Roxide < Formula
+  desc "Share your clipboard between different devices"
+  homepage "https://github.com/fioncat/csync"
+  url "https://github.com/fioncat/csync/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "todo"
+  license "MIT"
+  head "https://github.com/fioncat/csync.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/csync", "--version"
+  end
+end
