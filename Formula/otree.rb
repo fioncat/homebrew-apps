5033deab5f6451cb9b21c45e0abc026bf40383f4
fioncat <lazycat7706@gmail.com>
2024-12-26 15:55:43 +0800

    Update otree to 0.3.0

--- Formula/otree.rb
+++ Formula/otree.rb
@@ -1,8 +1,8 @@
 class Otree < Formula
   desc "A command line tool to view objects (JSON/YAML/TOML) in TUI tree widget."
   homepage "https://github.com/fioncat/otree"
-  url "https://github.com/fioncat/otree/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "todo"
+  url "https://github.com/fioncat/otree/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "63a63b09af2186d2bc3aa8631989f1fbdeb7659d7f1857be7fae1e0af4d42fde"
   license "MIT"
   head "https://github.com/fioncat/otree.git", branch: "main"
 
