08429e5f44b1ca0801085053af1861345783195d
fioncat <lazycat7706@gmail.com>
2024-12-26 14:13:50 +0800

    add roxide formula

---
+++ Formula/roxide.rb
@@ -0,0 +1,18 @@
+class Roxide < Formula
+  desc "Manage your git repositories."
+  homepage "https://github.com/fioncat/roxide"
+  url "https://github.com/fioncat/roxide/archive/refs/tags/v0.17.1.tar.gz"
+  sha256 "ba245d54ae21539a7fc7432df190dfffdfce1f6bcc0781bc1aa7aa10fb9eeb14"
+  license "MIT"
+  head "https://github.com/fioncat/roxide.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/roxide", "--version"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # homebrew-apps
-My persnal homebrew tap
+
+This a custom [Homebrew](https://brew.sh/) tap for my software.
+
+## Setup
+
+You can add the custom tap in a MacOS terminal session using:
+
+```bash
+brew tap fioncat/apps
+```
