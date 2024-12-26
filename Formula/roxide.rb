435ee31621f1751ec104e47cf8757f5b3e29f289
fioncat <lazycat7706@gmail.com>
2024-12-26 14:44:04 +0800

    test: tmp update roxide version

--- Formula/roxide.rb
+++ Formula/roxide.rb
@@ -1,7 +1,7 @@
 class Roxide < Formula
   desc "Manage your git repositories."
   homepage "https://github.com/fioncat/roxide"
-  url "https://github.com/fioncat/roxide/archive/refs/tags/v0.17.4.tar.gz"
+  url "https://github.com/fioncat/roxide/archive/refs/tags/v0.17.1.tar.gz"
   sha256 "8ddaf0da07320d7973b7e9490c654d153d6b61f84bad090108b6999a14799737"
   license "MIT"
   head "https://github.com/fioncat/roxide.git", branch: "main"
