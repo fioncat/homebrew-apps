6ddd8aa1b3f18a578985679234d94f60c08da0e3
fioncat <lazycat7706@gmail.com>
2024-12-26 15:25:45 +0800

    Update csync to 0.3.0

--- Formula/csync.rb
+++ Formula/csync.rb
@@ -1,8 +1,8 @@
 class Roxide < Formula
   desc "Share your clipboard between different devices"
   homepage "https://github.com/fioncat/csync"
-  url "https://github.com/fioncat/csync/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "todo"
+  url "https://github.com/fioncat/csync/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "56ecd98be630c9100584005e180bf2ce739c6ac686bce4b1275e3c6050f23628"
   license "MIT"
   head "https://github.com/fioncat/csync.git", branch: "main"
 
