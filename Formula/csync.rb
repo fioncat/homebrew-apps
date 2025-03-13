81085ba94d58a21ed2b56c37a9c55520f8cda5ad
fioncat <lazycat7706@gmail.com>
2025-03-13 16:55:10 +0800

    Update csync to 0.8.2

--- Formula/csync.rb
+++ Formula/csync.rb
@@ -1,8 +1,8 @@
 class Csync < Formula
   desc "Share your clipboard between different devices"
   homepage "https://github.com/fioncat/csync"
-  url "https://github.com/fioncat/csync/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "d8be049020858502595f187c66488d092ebad85d3bee1dfd5b11aba16a8f359d"
+  url "https://github.com/fioncat/csync/archive/refs/tags/v0.8.2.tar.gz"
+  sha256 "c4b73b9bdbf45bc4b3dcfecb8180d8240405d5678a272f6ce8da9f6eb4849f20"
   license "MIT"
   head "https://github.com/fioncat/csync.git", branch: "main"
 
