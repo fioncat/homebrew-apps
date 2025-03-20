191c3693f43ac25439806c8b66aa88dce97afa23
fioncat <lazycat7706@gmail.com>
2025-03-20 18:03:31 +0800

    Update roxide to 0.18.0

--- Formula/roxide.rb
+++ Formula/roxide.rb
@@ -1,8 +1,8 @@
 class Roxide < Formula
   desc "Manage your git repositories."
   homepage "https://github.com/fioncat/roxide"
-  url "https://github.com/fioncat/roxide/archive/refs/tags/v0.17.4.tar.gz"
-  sha256 "8ddaf0da07320d7973b7e9490c654d153d6b61f84bad090108b6999a14799737"
+  url "https://github.com/fioncat/roxide/archive/refs/tags/v0.18.0.tar.gz"
+  sha256 "d912c545e1caf7305d2d93cf540dcd49dd04f87d883172458c9ce3b12c805936"
   license "MIT"
   head "https://github.com/fioncat/roxide.git", branch: "main"
 
