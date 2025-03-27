d90add55ff030a342b36085fb075f5602377badd
fioncat <lazycat7706@gmail.com>
2025-03-27 16:57:24 +0800

    add fwd

---
+++ Formula/fwd.rb
@@ -0,0 +1,19 @@
+class Fwd < Formula
+  desc "Password-based symmetric encryption of files"
+  homepage "https://github.com/fioncat/filewarden"
+  url "https://github.com/fioncat/filewarden/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  license "MIT"
+  head "https://github.com/fioncat/filewarden.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release", "--locked"
+    bin.install "target/release/fwd"
+  end
+
+  test do
+    system "#{bin}/fwd", "-V"
+  end
+end
