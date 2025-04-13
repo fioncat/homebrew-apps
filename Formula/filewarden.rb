fd43e4f9f9be9130928171cf7b9342cecdca62f4
wenqian <lazycat7706@gmail.com>
2025-04-13 15:25:05 +0800

    Update filewarden.rb

--- Formula/filewarden.rb
+++ Formula/filewarden.rb
@@ -2,7 +2,7 @@ class Filewarden < Formula
   desc "Password-based symmetric encryption of files"
   homepage "https://github.com/fioncat/filewarden"
   url "https://github.com/fioncat/filewarden/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "de903de5e8fa3b6e687c4618e1727b1712f51f03966f4a9d069f420d6c7d2123"
   license "MIT"
   head "https://github.com/fioncat/filewarden.git", branch: "main"
 
