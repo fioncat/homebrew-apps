f981b12504fb15cf4d9c3efb31ec1c9d61db43a0
fioncat <lazycat7706@gmail.com>
2025-09-10 18:18:56 +0800

    update csync and otree

--- Formula/csync.rb
+++ Formula/csync.rb
@@ -1,18 +1,15 @@
 class Csync < Formula
   desc "Share your clipboard between different devices"
   homepage "https://github.com/fioncat/csync"
-  url "https://github.com/fioncat/csync/archive/refs/tags/v0.8.2.tar.gz"
-  sha256 "c4b73b9bdbf45bc4b3dcfecb8180d8240405d5678a272f6ce8da9f6eb4849f20"
+  url "https://github.com/fioncat/csync/releases/download/v0.8.3/csync-aarch64-apple-darwin.tar.gz"
+  sha256 "2c13000116454760dfaaa4f72c322aa7a4405beeae65e43fd4b44a5aad0ab36d"
   license "MIT"
   head "https://github.com/fioncat/csync.git", branch: "main"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "build", "--release", "--locked"
-    bin.install "target/release/csyncd"
-    bin.install "target/release/csynctl"
-    bin.install "target/release/csync-server"
+    bin.install "csyncd"
+    bin.install "csynctl"
+    bin.install "csync-server"
   end
 
   test do
--- Formula/otree.rb
+++ Formula/otree.rb
@@ -1,15 +1,13 @@
 class Otree < Formula
   desc "A command line tool to view objects (JSON/YAML/TOML) in TUI tree widget."
   homepage "https://github.com/fioncat/otree"
-  url "https://github.com/fioncat/otree/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "1c95f78c1b432b4a62392c971bc28eda8bc6754ec53e0701de1c42417c058bdf"
+  url "https://github.com/fioncat/otree/releases/download/v0.6.1/otree-aarch64-apple-darwin.tar.gz"
+  sha256 "c645da3b2873eb175d7e44d540fa9f8bb76ea3e775111472cc9caed613f91f24"
   license "MIT"
   head "https://github.com/fioncat/otree.git", branch: "main"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "otree"
   end
 
   test do
