8258556065e81c832493b127bec25cf5b81bb253
fioncat <lazycat7706@gmail.com>
2025-09-10 18:03:46 +0800

    update roxide

--- Formula/roxide.rb
+++ Formula/roxide.rb
@@ -1,16 +1,13 @@
 class Roxide < Formula
   desc "Manage your git repositories."
   homepage "https://github.com/fioncat/roxide"
-  url "https://github.com/fioncat/roxide/archive/refs/tags/v0.18.3.tar.gz"
-  sha256 "0d4e8f203c0455fd95b78022169441b15d5458b1b35a1a0cf94b2deaf36ecfe4"
+  url "https://github.com/fioncat/roxide/releases/download/v0.19.0/roxide-aarch64-apple-darwin.tar.gz"
+  sha256 "77220db3f148f0611634e686ed71265181a52cfccdfc09eb7524b83a6151d548"
   license "MIT"
   head "https://github.com/fioncat/roxide.git", branch: "main"
 
-  depends_on "go" => :build
-
   def install
-    system "make"
-    bin.install "bin/roxide"
+    bin.install "roxide"
   end
 
   test do
