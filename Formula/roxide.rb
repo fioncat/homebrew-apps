1c05ee06d5aaeba8d7a7e5230d04759b4fc6967e
fioncat <lazycat7706@gmail.com>
2025-09-19 15:23:36 +0800

    Update roxide to 0.20.2

--- Formula/roxide.rb
+++ Formula/roxide.rb
@@ -1,8 +1,8 @@
 class Roxide < Formula
   desc "Manage your git repositories."
   homepage "https://github.com/fioncat/roxide"
-  url "https://github.com/fioncat/roxide/releases/download/v0.20.1/roxide-aarch64-apple-darwin.tar.gz"
-  sha256 "74d803e5251fe4a894e6549537d3cbb6fb9edb199e2746a4af1c0d324e917345"
+  url "https://github.com/fioncat/roxide/releases/download/v0.20.2/roxide-aarch64-apple-darwin.tar.gz"
+  sha256 "73c03124dd55effeb394926047a69843e5364e4b1c1e9660211a820aba827351"
   license "MIT"
   head "https://github.com/fioncat/roxide.git", branch: "main"
 
