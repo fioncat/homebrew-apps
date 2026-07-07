9ada785bc4c705f24aec12c85f58576e999f7e3e
fioncat <lazycat7706@gmail.com>
2026-07-07 18:23:48 +0800

    Update otree to 0.7.1

--- Formula/otree.rb
+++ Formula/otree.rb
@@ -1,8 +1,8 @@
 class Otree < Formula
   desc "A command line tool to view objects (JSON/YAML/TOML) in TUI tree widget."
   homepage "https://github.com/fioncat/otree"
-  url "https://github.com/fioncat/otree/releases/download/v0.6.3/otree-aarch64-apple-darwin.tar.gz"
-  sha256 "4a184fcff0aa4a877484cef4708bd303eb9936c664929d2afccf0bedfcdeed49"
+  url "https://github.com/fioncat/otree/releases/download/v0.7.1/otree-aarch64-apple-darwin.tar.gz"
+  sha256 "f9047e35166dafbdda24c5acb95fda9410ac740a7feb83ca432d0d4624d7189c"
   license "MIT"
   head "https://github.com/fioncat/otree.git", branch: "main"
 
